b59df8019afe83e1e6a4281b30a86594f0de437b
kkk293590509 <k293590509@gmail.com>
2017-05-27 16:53:19 +0800

    empty cart

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -7,6 +7,9 @@ class Cart < ApplicationRecord
           ci.quantity = 1
           ci.save
      end
+      def clean!
+        cart_items.destroy_all
+     end
     def total_price
        sum = 0
        cart_items.each do |cart_item|
