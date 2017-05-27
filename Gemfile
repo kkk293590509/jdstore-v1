519b6a9f8216a1437cfcbbfd0f1ed2d13afb093e
kkk293590509 <k293590509@gmail.com>
2017-05-27 14:04:33 +0800

    add checkout routing

--- Gemfile
+++ Gemfile
@@ -35,7 +35,8 @@ gem 'jbuilder', '~> 2.5'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
- 
+gem 'aasm'
+
 gem 'carrierwave'
 gem 'mini_magick'
 group :development, :test do
--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,11 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :products
   end
-  resources :carts
-  
+   resources :carts do
+      collection do
+        delete :clean
+      end
+    end
   resources :products do
     member do
       post :add_to_cart
