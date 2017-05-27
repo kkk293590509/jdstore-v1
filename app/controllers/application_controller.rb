2b6dee47e29ef24c404bab3afc9f0c9b9888168d
kkk293590509 <k293590509@gmail.com>
2017-05-27 13:15:26 +0800

    implement current_cart function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,9 +10,22 @@ class ApplicationController < ActionController::Base
  devise_parameter_sanitizer.permit :account_update, keys: added_attrs
  end
 
-  def admin_required
-    if !current_user.admin?
-    redirect_to "/", alert: "You are not admin."
+   def admin_required
+       if !current_user.admin?
+          redirect_to "/", alert: "You are not admin."
+       end
    end
-end
+   helper_method :current_cart
+     def current_cart
+        @current_cart ||= find_cart
+     end
+     private
+     def find_cart
+           cart = Cart.find_by(id: session[:cart_id])
+        if cart.blank?
+           cart = Cart.create
+        end
+           session[:cart_id] = cart.id
+        return cart
+     end
 end
