11fbf8736c67033470cd478f0ea5df2303f0755a
superzrr <superzrr@zrr-MAC.local>
2017-06-01 05:51:22 +0800

    add pngs of views to asset/images and add to products/index.html.erb

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,14 +2,17 @@ class ProductsController < ApplicationController
     def index
         @products = Product.all
      end
-     def show
-         @product = Product.find(params[:id])
-     end
-       def add_to_cart
-           @product = Product.find(params[:id])
 
-         current_cart.add_product_to_cart(@product)
-         flash[:notice] = "成功加入购物车"
-         redirect_to :back
-        end
+     def show
+       if params[:id].to_i>1
+         @product = Product.find(params[:id])
+       end
+     end
+
+     def add_to_cart
+       @product = Product.find(params[:id])
+       current_cart.add_product_to_cart(@product)
+       flash[:notice] = "成功加入购物车"
+       redirect_to :back
+    end
 end
